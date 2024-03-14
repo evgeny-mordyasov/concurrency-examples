f63f229dd046e343af7a117cf9ef66946e36d1b6
evgeny-mordyasov <michisig11i7@gmail.com>
2024-03-14 23:12:40 +0700

    add example: sharing variables without sync

---
+++ concurrency-examples-7-sharing-variables-without-sync/src/main/java/rgo/concurrency/examples7/NoVisibility.java
@@ -0,0 +1,28 @@
+package rgo.concurrency.examples7;
+
+import rgo.concurrency.examples.annotations.NotThreadSafe;
+
+@NotThreadSafe
+public class NoVisibility {
+
+    private static boolean ready;
+    private static int number;
+
+    private static class ReaderThread extends Thread {
+
+        @Override
+        public void run() {
+            while (!ready) {
+                Thread.yield();
+            }
+
+            System.out.println(number);
+        }
+    }
+
+    public static void main(String[] args) {
+        new ReaderThread().start();
+        number = 42;
+        ready = true;
+    }
+}
